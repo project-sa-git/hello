14d9891c2b847b30619099767d4af3a8930bf92d
mac_sam <trybook1901@gmail.com>
2020-11-02 13:06:40 +0900

    ６章ファイル追加

---
+++ Shou_6.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Shou_6
+{
+    class Shou_6
+    {
+        static void Main(string[] args)
+        {
+            Switch();
+        }
+
+        
+        static void Switch()
+        {
+            // int month = DateTime.Now.Month; //現在の月を取得    
+            int month = 2; //現在の月
+
+            switch ( month )
+            {
+                case 1 :
+                    Console.WriteLine( "睦月" );
+                    break;
+                case 2 :
+                    Console.WriteLine( "如月" );
+                    break;
+                case 3 :
+                    Console.WriteLine( "弥生" );
+                    break;
+                case 7 :
+                case 8 :    // => 7, 8は同じ処理（省略）
+                    Console.WriteLine( "夏休み" );
+                    break;
+                default :
+                    Console.WriteLine( month + "月" );
+                    break;
+            }
+        }
+
+    }
+}
