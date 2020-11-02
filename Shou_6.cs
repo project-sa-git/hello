bdd280a350f1cc58d1c4be81899558dbe96f69f4
sam <trafics7642@gmail.com>
2020-11-02 13:35:23 +0900

    ６章途中

---
+++ Shou_6.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Shou_6
+{
+    class Shou_6
+    {
+        static void Main()
+        {
+            Car_test()
+        }
+        static void Car_test()
+        {
+            Car mycar = new Car();
+        }
+    }
+}
